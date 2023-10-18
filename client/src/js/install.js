a23a9d9d961cba8bf3515e2ad4ab9b2bb5c848c5
Ricky Carter <randolphcarter5@gmail.com>
2023-10-18 18:14:42 -0400

    created event listener before prompt installs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,3 +2,9 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Variable for storing beforeinstallprompt event
 let deferredPrompt;
+
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', async (event) => {
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
+});
