fd8c31a125803882911e6741a2656b16f879c8bf
Ricky Carter <randolphcarter5@gmail.com>
2023-10-18 17:50:13 -0400

    updated ad.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,3 +12,19 @@ const initdb = async () => {
     },
   });
 };
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ value: content });
+    const result = await request;
+  
+    if (result) {
+      console.log("Data saved to IndexedDB", result);
+    } else {
+      console.error("putDb not implemented");
+    }
+  };
+  
