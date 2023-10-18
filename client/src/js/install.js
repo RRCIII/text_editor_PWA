2bc739d6591b5644316a8c9ed4c1e6eddb096358
Ricky Carter <randolphcarter5@gmail.com>
2023-10-18 18:21:29 -0400

    created event listner for buttonInstall

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,3 +8,17 @@ window.addEventListener('beforeinstallprompt', async (event) => {
     deferredPrompt = event;
     butInstall.style.display = 'block';
 });
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const result = await deferredPrompt.userChoice;
+        if (result.outcome === 'accepted') {
+            console.log('PWA installation accepted');
+        }
+        console.log(deferredPrompt);
+        deferredPrompt = null;
+        butInstall.style.display = 'none';
+    }
+});
