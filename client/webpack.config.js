224e4879d5db49189875f81f7d31789aa3420260
Ricky Carter <randolphcarter5@gmail.com>
2023-10-29 15:07:45 -0400

    added loaders to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'J.A.T.E.',
+        template: './index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        publicPath: '/',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        start_url: '/',
+        display: 'standalone',
+        inject: true,
+        fingerprints: false,
+        orientation: 'portrait',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            "src": "./src/images/logo.png",
+            "sizes": [96, 128, 144, 152, 192, 256, 384, 512],
+            "destination": "./assets/icons"
+          } 
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
